dd41e69d4143ab16723f51d3c11316ceee8f4c75
Kotlubai <119881185+Kotlubai@users.noreply.github.com>
2023-02-27 00:51:23 +0200

    feat: add solution guessing-game task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.left = min
+        this.right = max
     }
 
     guess() {
-
+        this.middle = Math.round((this.right - this.left) / 2) + this.left
+        return this.middle
     }
 
     lower() {
-
+        this.right = this.middle
     }
 
     greater() {
-
+        this.left = this.middle
     }
 }
 
